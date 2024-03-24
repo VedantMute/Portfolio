7323e08cdf5c4273dba67520c2839247607041cb
TheVedant <vedantmute2004@gmail.com>
2024-03-24 17:31:07 +0530

    Final version released

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -17,7 +17,7 @@ export default function Footer() {
          Vedant
 {" "}
         </Link>
-        <p className="w-1/2 my-2 text-sm text-gray-500">Unwrapped your mind! <br /><span className="bg-indigo-50 dark:bg-gray-900 dark:text-indigo-300 inline-block border border-dashed border-indigo-600  px-2 ">Beta Version 0.0.11</span></p>
+        <p className="w-1/2 my-2 text-sm text-gray-500">Unwrapped your mind! <br /><span className="bg-indigo-50 dark:bg-gray-900 dark:text-indigo-300 inline-block border border-dashed border-indigo-600  px-2 ">Version 1.0.1</span></p>
       </div>
       <div className="grid grid-cols-2 gap-8 mt-12 xl:mt-0 xl:col-span-2">
         <div className="md:grid md:grid-cols-2 md:gap-8">
